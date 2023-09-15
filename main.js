ef8a381ac4fb78868cc819229e44a8226f51e6c4
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-15 21:53:53 +0500

    done

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 const minusBtn = document.querySelector('.product_minus')
 const plusBtn = document.querySelector('.product_plus')
+let totalPrice = document.querySelector('.total_price') 
 let num = document.querySelector('.product_number')
 
-document.addEventListener ("click" , minusBtn() {
-    num.innerHTML = `<p>${2}}</p>`
+let a = 0;
+let price = 0
+let num2 = 0
+
+plusBtn.addEventListener("click" , () => {
+  a++
+a = (a < 10) ? "0" + a : parseInt(a);
+  num.innerText = a
+price += 149.99
+
+totalPrice.innerText = `$ ${price}`
+  
+
 })
-// document.addEventListener("click", function(){
-//     document.body.style.backgroundColor = "red";
-//   });
+
+minusBtn.addEventListener("click" , () => {
+  a--
+  a= (a<=0) ? num2 : a;
+  num.innerText = a
+
+  price -= 149.99
+  price = (price < 0) ? num2 : price;
+
+totalPrice.innerText = `${price}`
+console.log(totalPrice);
+
+})
+
+
+
