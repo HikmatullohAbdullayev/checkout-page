0c6c38de513f0a9e582938e4ad5a09d70ab96245
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-15 21:05:04 +0500

    js start

---
+++ main.js
@@ -0,0 +1,10 @@
+const minusBtn = document.querySelector('.product_minus')
+const plusBtn = document.querySelector('.product_plus')
+let num = document.querySelector('.product_number')
+
+document.addEventListener ("click" , minusBtn() {
+    num.innerHTML = `<p>${2}}</p>`
+})
+// document.addEventListener("click", function(){
+//     document.body.style.backgroundColor = "red";
+//   });
